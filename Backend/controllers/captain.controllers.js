24e51c19f6e558a65a34c91feb3625a8dccb15bb
kavinash2j <killerapk66@gmail.com>
2025-06-26 07:34:44 +0530

    cookie-parser

--- Backend/controllers/captain.controllers.js
+++ Backend/controllers/captain.controllers.js
@@ -5,6 +5,7 @@ const { validationResult } = require("express-validator");
 
 
 module.exports.registerCaptain = async (req, res, next) => {
+
     const error = validationResult(req);
     if (!error.isEmpty()) {
         return res.status(400).json({ errors: error.array() });
@@ -13,7 +14,7 @@ module.exports.registerCaptain = async (req, res, next) => {
 
     const isCaptainAlready = await captainModel.findOne({ email });
     if (isCaptainAlready) {
-        res.status(400).json({ message: "captain All ready exist" })
+        res.status(400).json({ message: "captain All ready exist" });
     }
 
     const hashPassword = await captainModel.hashPassword(password);
@@ -38,29 +39,30 @@ module.exports.loginCaptain = async function (req, res, next) {
         return res.status(400).json({ errors: errors.array() });
     }
     const { email, password } = req.body;
-    console.log(email, password)
     const captain = await captainModel.findOne({ email }).select("+password");
+
     if (!captain) {
         res.status(500).json({ message: "Invalid username or password" });
     }
     const isMatch = await captain.comparePassword(password);
 
-    // console.log(isMatch);
     if (!isMatch) {
         res.status(500).json({ message: "Invalid username or password" });
-        console.log("invalid")
+        console.log("invalid");
     }
     const token = captain.generateAuthToken();
+
     res.cookie("token", token);
     res.status(200).json({ token, captain });
 }
 module.exports.logoutCaptian = async function (req, res, next) {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
     await blacklistTokenModel.create({ token });
+
     res.clearCookie('token');
     res.status(200).json({ meassage: "logout Successfully" });
 }
 module.exports.getCaptainProfile = async function (req, res, next) {
-    // console.log(req.captian);
     res.status(200).json(req.captian);
 }  
